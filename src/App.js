0932a3836bbd83f00592e326fb1b3f109a8019c4
JosefTaticek <jos.taticek@gmail.com>
2024-01-21 21:36:28 +0100

    Kata: #1 (Destructuring), #2 (useState), #3 (CSS flex-box styling)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {useState, useEffect} from "react"
+
 const App = () => {
-  return  (
-        <div></div>
-  )
+  
+  //1. Destructuring
+
+  const vychoziPole = ["David", "Hermiona", "Harry"]
+  const [student1, student2, student3] = vychoziPole
+
+  //2. UseState
+  const [title, setTitle] = useState("Původní text")
+  const buttonHandler = (()=>{
+    setTitle("Nový text")
+  })
+  
+  
+  return <div className="cta-button">
+    <h1>{title}</h1>
+    <button type="button" onClick={buttonHandler}>Změnit title</button>
+
+  </div>
 }
 
 
